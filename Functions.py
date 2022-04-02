1b607db7ae6e2f8dc0b7d94627c2c8a920f586e4
xShankyx <nshanklin22@yahoo.com>
2022-04-02 17:16:00 -0400

    Update

---
+++ Functions.py
@@ -0,0 +1,47 @@
+#def selectAsset():
+#     assets = ['btc','cash','gold','silver','spy']
+#     while True:
+#         asset = input('Please select an asset from {}: '.format(assets))
+#         if asset == "gold" or asset == "silver" or asset == "btc" or asset == "spy" or asset == "cash":
+#             break
+#         else:
+#             print("That is not a valid asset, please try again")
+#     return asset
+#
+# def assetValue(asset):
+#     while True:
+#         quantity = input('Please enter ammount of {} currently held in $: '.format(asset))
+#         if quantity.isnumeric():
+#             break
+#         else:
+#             print("That is not a valid ammount")
+#     return quantity
+#
+# def updateDf(df,asset,quantity):
+#     new_row = df.iloc[-1].copy()
+#     print(new_row.head())
+#     new_row['date'] = datetime.today().date()
+#     new_row[asset] = quantity
+#     print(new_row.head())
+#     # Append the main dataframe with the new row
+#     df = df.append(new_row, ignore_index=True)
+#     # Sort values by date
+#     df.sort_values('date', inplace=True)
+#     # Merge the data for duplicate dates
+#     df = df.groupby(['date'], as_index=False).max()
+#     return df
+#
+# def addNewEntry():
+#     global df
+#     asset = selectAsset()
+#     quantity = assetValue(asset)
+#     df = updateDf(df,asset,quantity)
+#     df.to_sql('PortfolioData', engine, if_exists='replace', index=False)
+
+def printMenu():
+    print("Menu Options")
+    print("-----------------------")
+    print("1 - Update assets")
+    print("2 - View data")
+    print("3 - Analyze data")
+    print("----------------------")
--- MainMenu.py
+++ MainMenu.py
@@ -5,7 +5,9 @@ import sqlalchemy
 import sqlite3
 import datetime
 from datetime import datetime
-#pd.options.mode.chained_assignment = None  # default='warn'
+from Functions import *
+
+# pd.options.mode.chained_assignment = None  # default='warn'
 
 # Create connection to SQL and an engine for SQLalchemy
 connection = sqlite3.connect('data\Portfolio.db')
@@ -14,45 +16,31 @@ engine = sqlalchemy.create_engine('sqlite:///data\Portfolio.db').connect()
 
 df = pd.read_sql_table('PortfolioData', engine, index_col=1)
 
-def selectAsset():
-    assets = ['btc','cash','gold','silver','spy']
+def mainMenu():
+    MenuOption = getMenuOption()
+    if(MenuOption == 1):
+        return
+    elif(MenuOption == 2):
+        return
+    elif(MenuOption == 3):
+        return
+
+def getMenuOption():
+    printMenu()
     while True:
-        asset = input('Please select an asset from {}: '.format(assets))
-        if asset == "gold" or asset == "silver" or asset == "btc" or asset == "spy" or asset == "cash":
-            break
+        MenuOption = input("Please select a menu option: ")
+        if MenuOption.isdigit() and (int(MenuOption) == 1 or int(MenuOption) == 2 or int(MenuOption) == 3):
+            return MenuOption
         else:
-            print("That is not a valid asset, please try again")
-    return asset
+            print("That is not a valid option")
 
-def assetValue(asset):
-    while True:
-        quantity = input('Please enter ammount of {} currently held in $: '.format(asset))
-        if quantity.isnumeric():
-            break
-        else:
-            print("That is not a valid ammount")
-    return quantity
+def updateAssets():
+    return
 
-def updateDf(df,asset,quantity):
-    new_row = df.iloc[-1].copy()
-    print(new_row.head())
-    new_row['date'] = datetime.today().date()
-    new_row[asset] = quantity
-    print(new_row.head())
-    # Append the main dataframe with the new row
-    df = df.append(new_row, ignore_index=True)
-    # Sort values by date
-    df.sort_values('date', inplace=True)
-    # Merge the data for duplicate dates
-    df = df.groupby(['date'], as_index=False).max()
-    return df
+def readData():
+    return
 
-def addNewEntry():
-    global df
-    asset = selectAsset()
-    quantity = assetValue(asset)
-    df = updateDf(df,asset,quantity)
-    df.to_sql('PortfolioData', engine, if_exists='replace', index=False)
+def analyzeData():
+    return
 
-
-addNewEntry()
+mainMenu()

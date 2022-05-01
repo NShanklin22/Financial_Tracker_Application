f9d62e7f26607d6713b795c72a1aae20cd9b1b1e
NShanklin22 <nshanklin22@yahoo.com>
2022-05-01 10:35:05 -0400

    updated

--- MainMenu.py
+++ MainMenu.py
@@ -20,22 +20,24 @@ AssetHoldings = pd.read_sql_table('AssetHoldings', engine, index_col='date')
 AssetValues = pd.read_sql_table('AssetValues', engine, index_col='date')
 
 global assets
-assets = ['btc_qty','cash_$','gold_g','silver_oz','spy_qty']
+assets = ['btc_qty', 'cash_$', 'gold_g', 'silver_oz', 'spy_qty']
 
-def mainMenu(AssetHoldings,AssetValues):
+
+def mainMenu(AssetHoldings, AssetValues):
     MenuOption = int(getMenuOption())
     if (MenuOption == 1):
-        updateHoldings(AssetHoldings,AssetValues)
+        updateHoldings(AssetHoldings, AssetValues)
         return
     elif (MenuOption == 2):
-        dataMenu(AssetHoldings,AssetValues)
+        dataMenu(AssetHoldings, AssetValues)
         return
     elif (MenuOption == 3):
         print("Lets analyze some data")
         analyzeData()
         return
 
-def updateHoldings(AssetHoldings,AssetValues):
+
+def updateHoldings(AssetHoldings, AssetValues):
     today = pd.to_datetime(datetime.today().date())
     AssetHoldings.at[today] = AssetHoldings.iloc[-1]
     match = False
@@ -48,7 +50,7 @@ def updateHoldings(AssetHoldings,AssetValues):
                     AssetHoldings.at[today, assets[i]] = quantity
                     print(AssetHoldings.tail(1))
                     response = input("Would you like to update another asset (Y/N)? ")
-                    if(response == 'Y'):
+                    if (response == 'Y'):
                         continue
                     else:
                         match = True
@@ -61,12 +63,13 @@ def updateHoldings(AssetHoldings,AssetValues):
     print("Holdings saved to database")
     print("Updating Asset Values...\n")
     time.sleep(2)
-    AssetValues = updateAssetValues(AssetValues,AssetHoldings)
+    AssetValues = updateAssetValues(AssetValues, AssetHoldings)
     print("Asset values updated successfully")
     print("Saving asset values to database...")
     time.sleep(2)
     AssetValues.to_sql('AssetValues', engine, if_exists='replace', index=True, index_label="date")
     print("Asset values saved to database!")
+
 
 def dataMenu(df):
     MenuOption = int(getDataMenu())
@@ -80,6 +83,7 @@ def dataMenu(df):
 
     mainMenu(df)
 
+
 def readData():
     return
 
@@ -87,4 +91,5 @@ def readData():
 def analyzeData():
     return
 
-mainMenu(AssetHoldings,AssetValues)
+
+mainMenu(AssetHoldings, AssetValues)
